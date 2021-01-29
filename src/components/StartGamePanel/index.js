ad8e133a4fb1f06f439321488fcd227da1d1000d
Damjan Piperski <damjanpiperski@gmail.com>
2021-01-29 15:08:46 +0100

    Ref for fund input on start game panel

--- src/components/StartGamePanel/index.js
+++ src/components/StartGamePanel/index.js
@@ -1,8 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './style.css';
 
 const StartGamePanel = ({setStartGame}) => {
   const [startAppear, setStartAppear] = useState(true);
+  const [readFundInput, setReadFundInput] = useState();
+  const [disableInput, setDisableInput] = useState(false);
+
+  const handleInputChange = () => {
+    setReadFundInput(fundInput.current.value);
+  }
+
+  const fundInput = useRef(null);
+
+  const clickPlay = () => {
+    var b = fundInput.current.value;
+    console.log(b)
+  }
+
   return (
     <div className="start-game-panel">
       <div className={`start-game-box ${startAppear ? 'start-game-appear' : 'start-game-disappear'}`}>
@@ -13,8 +27,7 @@ const StartGamePanel = ({setStartGame}) => {
           <div className="welcome-text">
             <p>This is a 5x3 slot game. Paytables include horizontal signs of two (2) and more being the same. 
               Gods are high symbols, animals are medium and objects are low (see legend on the top left). Bet One button increases the bet by one, Bet Max sets it to 10 which is the maximum, Clear Bet sets it to zero.
-              You can't spin without a
-
+              You can't spin without a bet.
             </p>
           </div>
           <div className="funds-area">
@@ -23,7 +36,15 @@ const StartGamePanel = ({setStartGame}) => {
             </div>
 
             <div className="funds-bar">
-              <input></input>
+              <input disabled={disableInput} onChange={e => {
+                
+                if (readFundInput > 9999) {
+                  e.preventDefault();  
+                } else {
+                  handleInputChange(e);
+                }
+              }
+                } ref={fundInput} type="number"></input>
             </div>
           </div>
         </div>
@@ -31,7 +52,8 @@ const StartGamePanel = ({setStartGame}) => {
           setStartAppear(false);
           setTimeout(() => {
           setStartGame(false);
-        }, 800)
+        }, 800);
+        clickPlay()
         }
         } className="play-button">
           PLAY
